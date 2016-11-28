3a9634e9839586cb7f26513feddcb7bfa1d9fe45
subin joseph <subin.joseph7@gmail.com>
2016-11-28 19:54:51 +0100

    added iperf server

--- chute/run.sh
+++ chute/run.sh
@@ -12,7 +12,7 @@ while true; do
     # NOT IMPLEMENTED
     #
 	iperf -s > /usr/share/nginx/html/results.txt
-	echo 'subin' > /usr/share/nginx/html/results.txt
+	#echo 'subin' > /usr/share/nginx/html/results.txt
 	
     sleep 60
 done
